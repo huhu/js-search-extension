3135638876e1c8ed24fc8961d8e0fbff225890e0
shwin0901 <401815708@qq.com>
2020-06-17 22:15:33 +0800

    Add html search docs

---
+++ extension/command/html.js
@@ -0,0 +1,36 @@
+class htmlCommand extends Command {
+    constructor() {
+        super("html", "html search docs!");
+        this.html = htmlIndex.map(([name, description]) => {
+            return {name, description}
+        })
+    }
+
+    onExecute(arg) {
+        let results = this.html;
+        if(arg) {
+            results = [];
+            for (let label of this.html) {
+                let index = label.name.toLowerCase().indexOf(arg);
+                if (index > -1) {
+                    label["matchIndex"] = index;
+                    results.push(label);
+                }
+            }
+
+            results = results.sort((a, b) => {
+                if (a.matchIndex === b.matchIndex) {
+                    return a.name.length - b.name.length;
+                }
+                return a.matchIndex - b.matchIndex;
+            });
+        }
+
+        return results.map((item) => {
+            return {
+                content: "https://developer.mozilla.org/" + item.description,
+                description: c.match(item.name)
+            }
+        })
+    }
+}
